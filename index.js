0552a2b529a32245d1fb5bb4831e48405e3855b6
Romain <romain.reghem@etud.univ-jfc.fr>
2022-11-30 17:18:30 +0100

    i

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 
-const bigbox = '{"type": "Polygon", "coordinates": [[[ 2.2927117966620396, 43.56198416615212 ], [ 2.2927117966620396, 43.651727421218276 ], [ 2.1991563462225865, 43.651727421218276 ], [ 2.1991563462225865, 43.56198416615212 ], [ 2.2927117966620396, 43.56198416615212 ] ] ]}'
-
 app.use(cors({
   origin: '*'
 }));
@@ -27,15 +25,22 @@ const eq_table = {
   zbio: { table: 'zbio', display: 'LBL_CULTU', search: false, dpt: null },
   riv: { table: 'riv', display: 'Libelle', search: true, dpt: null },
   pollu: { table: 'pollu', display: 'nom_site', search: true, dpt: 'code_dpt' },
+  inci: {table: 'inci', display:'exploitant', search:true, dpt:'c_dept'},
+  dechets:{table: 'dechets', display:'', search:true, dpt:null},
+  corse: {table:'corsebio', display:'nom_servic', search:false, dpt:null}
 }
 
 app.get('/api/layers', (req, res) => {
-  console.log(req.query.bounds)
-  console.log(bigbox)
+  // db.query(`SELECT * FROM ${eq_table[req.query.layer].table}`).then((data) => console.log(data))
   let where
+  let where2
   if (req.query.dpt && eq_table[req.query.layer].dpt && req.query.dpt != 1) {
     where = `WHERE ${eq_table[req.query.layer].dpt}::int=${req.query.dpt}`
   } else where = 'WHERE true'
+
+  if (req.query.layer == "zbio") {
+    where2 = `ST_Contains(ST_GeomFromGeoJSON('${req.query.bounds}'), ST_GeomFromGeoJSON(ST_AsGeoJSON(geom)))`
+  } else where2 = 'true'
   db.query(`SELECT jsonb_build_object(
     'type',     'FeatureCollection',
     'features', jsonb_agg(features.feature)
@@ -47,8 +52,9 @@ FROM (
     'geometry',   ST_AsGeoJSON(geom)::jsonb,
     'properties', to_jsonb(inputs) - 'gid' - 'geom'
   ) AS feature
-  FROM (SELECT * FROM ${eq_table[req.query.layer].table} ${where} AND ST_Contains(ST_GeomFromGeoJSON('${req.query.bounds}'), ST_GeomFromGeoJSON(ST_AsGeoJSON(geom))))  inputs) features;`)
+  FROM (SELECT * FROM ${eq_table[req.query.layer].table} ${where} AND ${where2})  inputs) features;`)
     .then((data) => {
+      // console.log(data)
       return res.send(data[0].jsonb_build_object)
     })
     .catch((error) => {
